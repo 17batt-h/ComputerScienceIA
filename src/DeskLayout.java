4d130c6570af32bb6b9a641cbb3d6c84d87b058e
17batt-h <17batt_h@bexleygs.co.uk>
2023-06-22 11:44:30 +0100

    22/6/23

--- src/DeskLayout.java
+++ src/DeskLayout.java
@@ -1,18 +1,36 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class DeskLayout extends JPanel {
     private static final int DESK_WIDTH = 80;
     private static final int DESK_HEIGHT = 40;
     private static final int SPACING = 20;
 
+    private BufferedImage backgroundImage;
+
     private int numDesks = 6;
+
+    public DeskLayout (){
+        try {
+            // Load the image from a file
+            backgroundImage = ImageIO.read(new File("C:\\Users\\17batt_h\\Downloads\\slayyyyy.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
+        if (backgroundImage != null) {
+            g.drawImage(backgroundImage, 0, 0,getWidth(), getHeight(),this);
+        }
 
         int panelWidth = getWidth();
         int panelHeight = getHeight();
